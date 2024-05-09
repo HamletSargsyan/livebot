0c433a4b7072fb8765265cd7ece40e437f3aa6df
hamletsargsyan <hamlets849@gmail.com>
2024-05-09 20:17:16 +0400

    Правки

---
+++ src/threads/check.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+from database.funcs import database
+from base.player import check_user_stats
+
+
+def check():
+    while True:
+        try:
+            users = database.users.get_all()
+
+            for user in users:
+                user = database.users.get(_id=user._id)
+                choice = random.randint(0, 2)
+                match choice:
+                    case 0:
+                        user.hunger += 1
+                    case 1:
+                        user.fatigue += 1
+                    case 2:
+                        user.mood -= 1
+
+                database.users.update(
+                    user._id,
+                    **{
+                        "hunger": user.hunger,
+                        "mood": user.mood,
+                        "fatigue": user.fatigue,
+                    },
+                )
+                check_user_stats(user)
+            time.sleep(3600)  # 1h
+        except Exception:
+            continue
---
+++ src/threads/notification.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from telebot.apihelper import ApiTelegramException
+from telebot.util import antiflood, quick_markup
+
+from database.funcs import database
+from helpers.exceptions import NoResult
+from config import bot
+
+
+def notification():
+    while True:
+        try:
+            users = database.users.get_all()
+
+            for user in users:
+                try:
+                    user_notification = database.notifications.get(
+                        **{"owner": user._id}
+                    )
+                except NoResult:
+                    continue
+                user = database.users.get(_id=user._id)
+                try:
+                    current_time = datetime.utcnow()
+                    if user.action_time <= current_time:
+                        if user.state == "street" and not user_notification.walk:
+                            user_notification.walk = True
+                            mess = "Ты закончил прогулку"
+                        elif user.state == "work" and not user_notification.work:
+                            user_notification.work = True
+                            mess = "Ты закончил работу"
+                        elif user.state == "sleep" and not user_notification.sleep:
+                            user_notification.sleep = True
+                            mess = "Ты проснулся"
+                        else:
+                            continue
+
+                        markup = quick_markup(
+                            {"Дом": {"callback_data": f"open home {user.id}"}}
+                        )
+                        antiflood(bot.send_message, user.id, mess, reply_markup=markup)
+                except ApiTelegramException:
+                    continue
+
+                database.notifications.update(**user_notification.to_dict())
+        except Exception:
+            continue
