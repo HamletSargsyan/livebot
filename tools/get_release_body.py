922304bccf9e451a72a1f7e78394290e0b1d4b66
Hamlet <hamlets849@gmail.com>
2024-07-22 13:35:29 +0400

    правки

--- tools/get_release_body.py
+++ tools/get_release_body.py
@@ -1,9 +1,15 @@
+import re
 import sys
 import changelog
 
 with open("CHANGELOG.md") as f:
     changes = changelog.load(f)[1]
 
-# print(changelog.dumps([changes], "").strip())
-sys.stdout.write(changelog.dumps([changes], "").strip())
+
+content = changelog.dumps([changes], "").strip()
+
+if match := re.match(r"## \[\d+\.\d+\.\d+\] - \d{4}-\d{2}-\d{2}", content):
+    content = content.replace(match.group(0), "").strip()
+
+sys.stdout.write(content)
 sys.stdout.flush()
