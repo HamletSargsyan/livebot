57b57bb15d93e968a9ec9ca4cd7a71f998cbce33
Hamlet <hamlets849@gmail.com>
2024-07-24 17:40:13 +0400

    правки

--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -2,13 +2,11 @@ from typing import Type
 from telebot.handler_backends import BaseMiddleware
 
 from .register import RegisterMiddleware
-from .advert import AdvertMiddleware
 from .actives import ActiveMiddleware
 
 
 middlewares: list[Type[BaseMiddleware]] = [
     RegisterMiddleware,
-    AdvertMiddleware,
     ActiveMiddleware,
 ]
 
